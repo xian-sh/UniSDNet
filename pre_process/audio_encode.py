ae203bb3945c8a01a142d265bee48dd4b93eee44
Compute-Chem <58101737+xianHUJINGJING@users.noreply.github.com>
2023-08-29 10:46:40 +0800

    Update audio_encode.py

--- pre_process/audio_encode.py
+++ pre_process/audio_encode.py
@@ -1,9 +1,52 @@
-import numpy as np
 import librosa
 import torch
 import os
 from tqdm import tqdm
-from transformers import AutoProcessor, Data2VecAudioModel
+from transformers import AutoProcessor, Wav2Vec2Model, Data2VecAudioModel, ASTModel, UniSpeechSatModel
+
+
+class Wav2vec2Model(torch.nn.Module):
+    def __init__(self):
+        super(Wav2vec2Model, self).__init__()
+
+        self.model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-base-960h")
+        self.processor = AutoProcessor.from_pretrained("facebook/wav2vec2-base-960h")
+        self.norm = torch.nn.LayerNorm(768, eps=1e-05, elementwise_affine=True)
+        self.avgpool = torch.nn.AdaptiveAvgPool1d(output_size=1)
+
+    def forward(self, data, sr):
+        inputs = self.processor(data, sampling_rate=sr, return_tensors="pt")
+        with torch.no_grad():
+            x = self.model(**inputs)
+            x = x.last_hidden_state
+            x = self.norm(x)
+            x = self.avgpool(x.transpose(1, 2))
+            x = x.transpose(1, 2).squeeze(0)  # (1, 1, 768) -> (1, 768)
+            # print(x.shape)
+            return x
+
+
+# wav2vec2
+def audio_wav2vec2_feat_extract(audios_path, aid_dir):
+
+    audio_model = Wav2vec2Model()
+
+    if not os.path.exists(aid_dir):
+        os.mkdir(aid_dir)
+
+    file_list = os.listdir(audios_path)
+    for file in tqdm(file_list):
+        save_path = os.path.join(aid_dir, file.split(".")[0] + '.pt')
+        # print(save_path)
+        if os.path.exists(save_path):
+            continue
+
+        audio_data, _ = librosa.load(os.path.join(audios_path, file), sr=16000)  # sample rate should be 16000
+        # print(audio_data.shape)
+        outputs = audio_model(audio_data, sr=16000)
+
+        torch.save(outputs, save_path)
+
 
 class Data2vecModel(torch.nn.Module):
     def __init__(self):
@@ -21,8 +64,9 @@ class Data2vecModel(torch.nn.Module):
             x = x.last_hidden_state
             x = self.norm(x)
             x = self.avgpool(x.transpose(1, 2))
-            x = x.transpose(1, 2)
-            return x  # (1,1,678)
+            x = x.transpose(1, 2).squeeze(0)  # (1, 1, 768) -> (1, 768)
+            # print(x.shape)
+            return x
 
 
 # data2vec
@@ -33,9 +77,83 @@ def audio_data2vec_feat_extract(audios_path, aid_dir):
     if not os.path.exists(aid_dir):
         os.mkdir(aid_dir)
 
-    # 遍历目标目录及其子目录
+    file_list = os.listdir(audios_path)
+    for file in tqdm(file_list):
+        save_path = os.path.join(aid_dir, file.split(".")[0] + '.pt')
+        # print(save_path)
+        if os.path.exists(save_path):
+            continue
+
+        audio_data, _ = librosa.load(os.path.join(audios_path, file), sr=16000)  # sample rate should be 16000
+        # print(audio_data.shape)
+        outputs = audio_model(audio_data, sr=16000)
+
+        torch.save(outputs, save_path)
+
+
+class ASTaudioModel(torch.nn.Module):
+    def __init__(self):
+        super(ASTaudioModel, self).__init__()
+
+        self.model = ASTModel.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
+        self.processor = AutoProcessor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
+        self.norm = torch.nn.LayerNorm(768, eps=1e-05, elementwise_affine=True)
+        self.avgpool = torch.nn.AdaptiveAvgPool1d(output_size=1)
+
+    def forward(self, data, sr):
+        inputs = self.processor(data, sampling_rate=sr, return_tensors="pt")
+        with torch.no_grad():
+            x = self.model(**inputs)
+            x = x.last_hidden_state
+            x = self.norm(x)
+            x = self.avgpool(x.transpose(1, 2))
+            x = x.transpose(1, 2)
+            return x
+
+# ast
+def audio_ast_feat_extract(audios_path, aid_dir):
+
+    audio_model = ASTaudioModel()
+    if not os.path.exists(aid_dir):
+        os.mkdir(aid_dir)
+
     for root, dirs, files in os.walk(audios_path):
-        # 遍历当前目录中的所有文件
+        for file in tqdm(files):
+            save_path = os.path.join(aid_dir, file.split(".")[0] + '.pt')
+            if os.path.exists(save_path):
+                continue
+
+            audio_data, _ = librosa.load(os.path.join(root, file), sr=16000)  # sample rate should be 16000
+            outputs = audio_model(audio_data, sr=16000)
+
+            torch.save(outputs, save_path)
+
+class UniSpeechModel(torch.nn.Module):
+    def __init__(self):
+        super(UniSpeechModel, self).__init__()
+
+        self.model = UniSpeechSatModel.from_pretrained("microsoft/unispeech-sat-base-100h-libri-ft")
+        self.processor = AutoProcessor.from_pretrained("microsoft/unispeech-sat-base-100h-libri-ft")
+        self.norm = torch.nn.LayerNorm(768, eps=1e-05, elementwise_affine=True)
+        self.avgpool = torch.nn.AdaptiveAvgPool1d(output_size=1)
+
+    def forward(self, data, sr):
+        inputs = self.processor(data, sampling_rate=sr, return_tensors="pt")
+        with torch.no_grad():
+            x = self.model(**inputs)
+            x = x.last_hidden_state
+            x = self.norm(x)
+            x = self.avgpool(x.transpose(1, 2))
+            x = x.transpose(1, 2)
+            return x
+
+# unispeech
+def audio_unispeech_feat_extract(audios_path, aid_dir):
+
+    audio_model = UniSpeechModel()
+    if not os.path.exists(aid_dir):
+        os.mkdir(aid_dir)
+    for root, dirs, files in os.walk(audios_path):
         for file in tqdm(files):
             save_path = os.path.join(aid_dir, file.split(".")[0] + '.pt')
             if os.path.exists(save_path):
@@ -49,7 +167,8 @@ def audio_data2vec_feat_extract(audios_path, aid_dir):
 
 if __name__ == '__main__':
     audios_path = 'H:/Code/SVG/data/wavs_16k'
-    aid_dir = r'.\data\audio_data2vec_feat'  # save path
 
+    aid_dir = r'path\TACoS\audio_data2vec_feat'
+    aid_dir1 = r'path\TACoS\audio_wav2vec2_feat'
     audio_data2vec_feat_extract(audios_path, aid_dir)
-
+    audio_wav2vec2_feat_extract(audios_path, aid_dir1)
